9a0c2114c2301198ee825b4be059bf1aae21a6ea
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-18 04:02:34 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,28 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
-  puts phrase
+  phrase = "It's-a me, Mario!"
+  puts "#{phrase}"
 end
 
+mario
+
 def toadstool
-  puts status
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts "#{status}"
 end
+
+toadstool
+
+def link
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+link
+
+def any_phrase(phrase)
+  puts "#{phrase}"
+end
+
+phrase = "Do A Barrell Roll!"
+
+
+any_phrase(phrase)
